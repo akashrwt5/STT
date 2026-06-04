f7aebeb969277f138ba76ef64050cd26660a4b16
Akash Rawat <akash_rawat@starkey.com>
2026-06-04 15:21:05 +0530

    Initial commit

---
+++ STT/STTApp.swift
@@ -0,0 +1,17 @@
+//
+//  STTApp.swift
+//  STT
+//
+//  Created by Akash Rawat on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct STTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ STTTests/STTTests.swift
@@ -0,0 +1,16 @@
+//
+//  STTTests.swift
+//  STTTests
+//
+//  Created by Akash Rawat on 04/06/26.
+//
+
+import Testing
+
+struct STTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
